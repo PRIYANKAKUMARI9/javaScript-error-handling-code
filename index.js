71c0124cbf4b4daf9c830fc02e83a29c255c8530
Priyanka Sharma <108454203+PRIYANKAKUMARI9@users.noreply.github.com>
2024-01-06 21:33:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+try{
+let aa=prompt("hello im priyanka");
+
+if(aa=='y'){
+    throw{
+        name:'priyanka'
+    }
+}
+}
+catch(err){
+    console.log(err)
+
+}
+
+
+
+
+
+
+
+
+/*try{
+console.log(age);
+}
+catch(e){
+    console.log(e.name);
+    console.log(e.message);
+}
+
+let a=4*8;
+console.log("4*8 =", a);*/
